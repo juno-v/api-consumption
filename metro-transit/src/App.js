cea3f6adc7fc9b92825d960c17d94462f0b4c6ba
Juno Vue <juno.vue@hotmail.com>
2019-08-15 19:08:19 -0500

    created a component to fetchadata

--- metro-transit/src/App.js
+++ metro-transit/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import FetchData from "./FetchData/FetchaData"; 
 
 function App() {
   return (
     <div>
      <h1> hello world </h1>
+     <FetchData /> 
     </div>
   );
 }
---
+++ metro-transit/src/FetchData/FetchaData.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FetchData extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>fetch data here</h1> 
+            </div>
+         );
+    }
+}
+ 
+export default FetchData;
