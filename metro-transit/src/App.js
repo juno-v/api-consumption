f4db1e44a0b4822951b6e405e9a1b768de3bd9f9
Juno Vue <juno.vue@hotmail.com>
2019-08-17 01:12:25 -0500

    installed scss dependencies for styling and created a Header component to test other code.

--- metro-transit/src/App.js
+++ metro-transit/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from "./components/Header/Header"; 
 import FetchData from "./FetchData/FetchaData"; 
 
 function App() {
   return (
     <div>
+      <Header/>
      <h1> hello world </h1>
      <FetchData /> 
     </div>
---
+++ metro-transit/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1> Header </h1>
+            </div>
+         );
+    }
+}
+ 
+export default Header;
