c26e6f9794cc487f1e198b1779451717f5db8253
Juno Vue <juno.vue@hotmail.com>
2019-08-17 02:35:42 -0500

    headline component green tests

--- metro-transit/src/App.js
+++ metro-transit/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/Header/Header"; 
-
+import Headline from "./components/Headline/Headline"; 
 function App() {
   return (
     <div>
       <Header/>
-     <h1> hello world </h1>
+
+      <section className="main">
+        <Headline header="Posts" description="Click the button to render posts" /> 
+      </section>
+      
+     <h1> APP.JS </h1>
     </div>
   );
 }
