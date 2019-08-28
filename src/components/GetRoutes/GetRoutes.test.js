46652163c1bdd37157d98aea4ebee9c32f3c8808
Juno Vue <Juno.Vue@Hotmail.com>
2019-08-28 00:37:42 -0500

    green test for GetRoutes

--- src/components/GetRoutes/GetRoutes.test.js
+++ src/components/GetRoutes/GetRoutes.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import GetDirections from "./GetDirections"; 
+import GetRoutes from "./GetRoutes"; 
 import { findDataTest } from "./../../../Utilities/Utilities"; 
 
 describe(`getDirections Component`, () => {
     it(`It should render without any errors`, () => {
-    const component = shallow(<GetDirections />);
-    const wrapper = findDataTest(component, `fetchDirectionContainerDiv`);
+    const component = shallow(<GetRoutes />);
+    const wrapper = findDataTest(component, `fetchDataContainerDiv`);
     // expect(wrapper.length).toBe(2); // this test will fail
     expect(wrapper.length).toBe(1); 
     })
